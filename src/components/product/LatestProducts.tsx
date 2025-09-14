725078332bea0fba0274f877597dbbb1894060b3
draftworks-repo <draftworks000@gmail.com>
2025-09-14 17:00:52 +0530

    Hover Preview Image Removed

--- src/components/product/LatestProducts.tsx
+++ src/components/product/LatestProducts.tsx
@@ -61,7 +61,7 @@ function ProductCard({ product }: { product: (typeof products)[0] }) {
     <div className={styles.card}>
       {/* Make the image clickable */}
       <Link href={`/product/${product.id}`} className={styles.imageLink}>
-        <HoverPreviewWrapper previewImage={product.image}>
+        {/* <HoverPreviewWrapper previewImage={product.image}> */}
           <div
             className={styles.imageWrapper}
             onMouseEnter={() => setHovered(true)}
@@ -78,7 +78,7 @@ function ProductCard({ product }: { product: (typeof products)[0] }) {
               className={`${styles.image} ${hovered ? styles.fadeIn : styles.fadeOut}`}
             />
           </div>
-        </HoverPreviewWrapper>
+        {/* </HoverPreviewWrapper> */}
       </Link>
 
       {/* Info stays outside */}
